f833cfa42c5860c9d5149b1d61ab7d72238372eb
Stefan Lambić <84741877+StefanLambic@users.noreply.github.com>
2023-02-13 17:37:52 +0100

    Initial Commit.
    Adding all the files.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//Menu
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("move");
+  navbar.classList.toggle("open-menu");
+});
+
+// Swiper
+var swiper = new Swiper(".services-content", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 50000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//Header
+let header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
